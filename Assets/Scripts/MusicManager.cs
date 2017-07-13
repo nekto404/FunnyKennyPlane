f15624469bc7ac32dd5d7b822c2a8978d11833c1
Unknown <gsmartrsh@gmail.com>
2017-07-13 19:20:02 +0300

    Add music in game

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour
+{
+    public Sprite MusicOn;
+    public Sprite MusicOff;
+
+    public Image MusicButton;
+
+    private int _music = 1;
+
+	void Start ()
+	{
+        Init();
+	}
+	
+    void Init()
+    {
+        if (PlayerPrefs.HasKey("Music"))
+        {
+            _music = PlayerPrefs.GetInt("Music");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Music", _music);
+        }
+        MusicButton.sprite = (_music != 0) ? MusicOn : MusicOff;
+    }
+
+    public void ChangeState()
+    {
+        _music = (_music != 0) ? 0 : 1;
+        PlayerPrefs.SetInt("Music", _music);
+        MusicButton.sprite = (_music != 0) ? MusicOn : MusicOff;
+    }
+
+    public bool GetMusicState()
+    {
+        return (_music != 0) ? true : false;
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+
+    private AudioSource _musicPlayer;
+    public MusicManager MusicM;
+    void Start () {
+        _musicPlayer = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void Play()
+    {
+        if (MusicM.GetMusicState())
+        {
+            _musicPlayer.Play();
+        }
+    }
+
+
+}
